854194311b3780023056838b5f69b48b21eb09e2
rotaliator <rotaliator@gmail.com>
2019-02-14 11:15:15 +0100

    more, more, more

--- olx_beholder.py
+++ olx_beholder.py
@@ -1,8 +1,24 @@
 import sqlite3
+from collections import namedtuple
+import configparser
 import requests
 from bs4 import BeautifulSoup
+from pyhiccup.core import html
 from cache_local import cache_local
+from send_email import send_email_two_part
 
+
+config = configparser.ConfigParser()
+config.read('olx_beholder.ini')
+sender = config['Message']['sender']
+receiver = config['Message']['receiver']
+bcc = config['Message'].get('bcc', '')
+subject= config['Message']['subject']
+
+
+Offer = namedtuple("Offer", "title link city price")
+
+offers_to_send = []
 
 def init_db(conn):
     conn.execute('''CREATE TABLE IF NOT EXISTS offers
@@ -15,8 +31,27 @@ def insert_offer(conn, title, link, city, price):
                      (title, link, city, price))
         conn.commit()
         print("new OFFER!")
+        offers_to_send.append(Offer(title, link, city, price))
     except sqlite3.IntegrityError:
         pass
+
+def format_body_text(offers):
+    body_text = "\n".join(f'* {o.city} {o.price} {o.title} {o.link}\n' for o in offers)
+    return body_text
+
+def format_body_html(offers):
+    margin_right = {"style": "margin-right: 3px;"}
+    data = [
+        ['div',
+         ['ul',
+          [['li',
+            ['i', margin_right, o.city], ['b', margin_right, o.price], ['a', {'href': o.link}, o.title]]
+           for o in offers]]
+        ]
+    ]
+    body_html = html(data)
+    body_html = body_html.replace('dir="rtl"', 'dir="ltr"')
+    return body_html
 
 
 def process_results(conn, content):
@@ -52,6 +87,12 @@ def main():
         for url in urls:
             results = get_results(url)
             process_results(conn, results)
+
+    if offers_to_send:
+        body_text = format_body_text(offers_to_send)
+        body_html = format_body_html(offers_to_send)
+        print(body_text, "\n", body_html)
+        send_email_two_part(receiver, sender, subject, body_text, body_html, bcc)
 
 
 if __name__ == '__main__':
--- send_email.py
+++ send_email.py
@@ -1,13 +1,16 @@
 import configparser
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 
 config = configparser.ConfigParser()
-config.read('send_email.ini')
-username = config['DEFAULT']['username']
-passwd = config['DEFAULT']['passwd']
-smtp_host = config['DEFAULT']['smtp_host']
-port = config['DEFAULT']['port']
-use_tls = config['DEFAULT']['use_tls']
+config.read('olx_beholder.ini')
+username = config['Smtp']['username']
+passwd = config['Smtp']['passwd']
+smtp_host = config['Smtp']['smtp_host']
+port = config['Smtp']['port']
+use_tls = config['Smtp']['use_tls']
 
 def send_email(receiver, sender, subject, body, bcc=''):
     print('sender: {sender}, receiver: {receiver}'.
@@ -23,6 +26,35 @@ def send_email(receiver, sender, subject, body, bcc=''):
             smtp.ehlo
         smtp.login(username, passwd)
         smtp.sendmail(sender, [receiver] + [bcc], message.encode())
+        print('Successfully sent email to {receiver}'
+              .format(receiver=receiver))
+    except smtplib.SMTPException as e:
+        print('Error sending email!', e)
+
+
+def send_email_two_part(receiver, sender, subject, body_text, body_html, bcc=''):
+    print('sender: {sender}, receiver: {receiver}'.
+          format(sender=sender, receiver=receiver))
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = receiver
+
+    part1 = MIMEText(body_text, 'plain')
+    part2 = MIMEText(body_html, 'html')
+
+    msg.attach(part1)
+    msg.attach(part2)
+
+    try:
+        smtp = smtplib.SMTP(smtp_host, port)
+        smtp.ehlo()
+        if (use_tls == 'yes'):
+            smtp.starttls()
+            smtp.ehlo
+        smtp.login(username, passwd)
+        smtp.sendmail(sender, [receiver] + [bcc], msg.as_string())
         print('Successfully sent email to {receiver}'
               .format(receiver=receiver))
     except smtplib.SMTPException as e:
